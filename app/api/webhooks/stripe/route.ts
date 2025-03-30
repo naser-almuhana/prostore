0b064d6bce73ea69a9e3ee69d572117afb820a8b
naseralmuhana <naseralmuhana0@gmail.com>
2025-03-30 18:52:02 +0300

    fix stripe route payment

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -5,38 +5,34 @@ import Stripe from "stripe"
 import { updateOrderToPaid } from "@/lib/actions/order.actions"
 
 export async function POST(req: NextRequest) {
-  try {
-    // Build the webhook event
-    const event = Stripe.webhooks.constructEvent(
-      await req.text(),
-      req.headers.get("stripe-signature") as string,
-      process.env.STRIPE_WEBHOOK_SECRET as string,
-    )
+  // Build the webhook event
+  const event = await Stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string,
+  )
 
-    // Check for successful payment
-    if (event.type === "charge.succeeded") {
-      const { object } = event.data
+  // Check for successful payment
+  if (event.type === "charge.succeeded") {
+    const { object } = event.data
 
-      // Update order status
-      await updateOrderToPaid({
-        orderId: object.metadata.orderId,
-        paymentResult: {
-          id: object.id,
-          status: "COMPLETED",
-          email_address: object.billing_details.email!,
-          pricePaid: (object.amount / 100).toFixed(),
-        },
-      })
-
-      return NextResponse.json({
-        message: "updateOrderToPaid was successful",
-      })
-    }
+    // Update order status
+    await updateOrderToPaid({
+      orderId: object.metadata.orderId,
+      paymentResult: {
+        id: object.id,
+        status: "COMPLETED",
+        email_address: object.billing_details.email!,
+        pricePaid: (object.amount / 100).toFixed(),
+      },
+    })
 
     return NextResponse.json({
-      message: "event is not charge.succeeded",
+      message: "updateOrderToPaid was successful",
     })
-  } catch (error) {
-    console.log(`⚠️  Webhook signature verification failed.`, error)
   }
+
+  return NextResponse.json({
+    message: "event is not charge.succeeded",
+  })
 }
